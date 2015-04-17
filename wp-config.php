e2f162ea95ed028513b4ffe063966f3515c07cb4
James Kontargyris <james@jameskontargyris.co.uk>
2015-04-17 10:36:23 +0000

    Environment-specific config updates

--- wp-config.php
+++ wp-config.php
@@ -18,6 +18,55 @@ if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
     include(dirname(__FILE__) . '/local-config.php');
     define('WP_LOCAL_DEV', true);
 }
+else {
+    // ** MySQL settings - You can get this info from your web host ** //
+    /** The name of the database for WordPress */
+    define('DB_NAME', $_ENV['DB_NAME']);
+
+    /** MySQL database username */
+    define('DB_USER', $_ENV['DB_USER']);
+
+    /** MySQL database password */
+    define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
+
+    /** MySQL hostname */
+    define('DB_HOST', $_ENV['DB_HOST']);
+
+    /** Database Charset to use in creating database tables. */
+    define('DB_CHARSET', $_ENV['DB_CHARSET']);
+
+    /** The Database Collate type. Don't change this if in doubt. */
+    define('DB_COLLATE', '');
+
+    /**#@+
+     * Authentication Unique Keys and Salts.
+     *
+     * Change these to different unique phrases!
+     * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+     * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+     *
+     * @since 2.6.0
+     */
+    define('AUTH_KEY',         $_ENV['AUTH_KEY']);
+    define('SECURE_AUTH_KEY',  $_ENV['SECURE_AUTH_KEY']);
+    define('LOGGED_IN_KEY',    $_ENV['LOGGED_IN_KEY']);
+    define('NONCE_KEY',        $_ENV['NONCE_KEY']);
+    define('AUTH_SALT',        $_ENV['AUTH_SALT']);
+    define('SECURE_AUTH_SALT', $_ENV['SECURE_AUTH_SALT']);
+    define('LOGGED_IN_SALT',   $_ENV['LOGGED_IN_SALT']);
+    define('NONCE_SALT',       $_ENV['NONCE_SALT']);
+
+    /**#@-*/
+
+    /**
+     * For developers: WordPress debugging mode.
+     *
+     * Change this to true to enable the display of notices during development.
+     * It is strongly recommended that plugin and theme developers use WP_DEBUG
+     * in their development environments.
+     */
+    define('WP_DEBUG', false);
+}
 
 /**#@-*/
 
@@ -28,15 +77,6 @@ if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
  * prefix. Only numbers, letters, and underscores please!
  */
 $table_prefix  = 'wp_';
-
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- */
-define('WP_DEBUG', false);
 
 /* That's all, stop editing! Happy blogging. */
 
